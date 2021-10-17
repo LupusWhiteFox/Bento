56b134c98badb7f42628db090023f68ebbe149b9
白いキツネのラプス <observative2oppp@gmail.com>
2021-10-17 18:12:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -23,12 +23,12 @@ const CONFIG = {
   greetingNight: 'Go to Sleep!',
 
   // Weather
-  weatherKey: 'InsertYourAPIKeyHere123456',
+  weatherKey: 'a7f9406cc6f58dc1bb5d6cda701dde94',
   weatherIcons: 'OneDark', // 'Nord', 'Dark', 'White'
   weatherUnit: 'C', // 'F', 'C'
   language: 'en', // More languages in https://openweathermap.org/current#multi
 
-  trackLocation: false, // If false or an error occurs, the app will use the lat/lon below
+  trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
   defaultLatitude: '37.775',
   defaultLongitude: '-122.419',
 
